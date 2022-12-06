bed71d76bf4ec225cda7f84e90c90c0478864edc
spacetime-ecology <ben.seliger@gmail.com>
2022-12-06 14:45:16 -0500

    updated to pull files from shared drive

--- get_Envi.R
+++ get_Envi.R
@@ -1,11 +1,13 @@
-get_Envi <- function(bio=T, lc=T, rnr=T, soil=T, res=1){#borders=NULL, pop=T, crop=T,
-  require(terra)
+get_Envi <- function(bio=F, elev=F, gdd=F, lc=F, pop=F, ptime=F, rnr=F, soil=F, tbase=5, res=1){
   require(geodata)
+  require(raster)
+  require(terra)
+  
+  #geodir <- "C:\\Users\\bjselige\\Documents\\geodata\\"
+  geodir <- 'Q:\\Shared drives\\Data\\Raster\\Global\\'
   
   if(bio==T){
-    biodir <- 'C:\\Users\\bjselige\\Documents\\localdata\\wc2.0_30s_bio\\'
-    #biodir <- 'Q:\\Shared drives\\APHIS  Projects\\shared resources\\data\\worldclim1k\\US\\'
-    biovar <- rast(list.files(biodir, full.names=T))
+    biovar <- worldclim_global(var='bio', res=.5, path=geodir)
     names(biovar) <-  c('Mean Annual Temp', 'Mean Diurnal Range', 'Isothermality', 'Temp Seasonality',
                         'Max Temp Warmest Month', 'Min Temp Coldest Month', 'Temp Annual Range',
                         'Mean Temp Wettest Quarter', 'Mean Temp Driest Quarter', 'Mean Temp Warmest Quarter',
@@ -27,56 +29,86 @@ get_Envi <- function(bio=T, lc=T, rnr=T, soil=T, res=1){#borders=NULL, pop=T, cr
     biocl$cluster[which(biocl$var%in%c('Annual Precip',
                                        'Precip Wettest Month',
                                        'Precip Wettest Quarter',
-                                       'Precip Warmest Quarter'))] <- 'Precip 1'
+                                       'Precip Warmest Quarter',
+                                       'Precip Coldest Quarter'))] <- 'Precip 1'
     biocl$cluster[which(biocl$var%in%c('Precip Seasonality',
                                        'Precip Driest Month',
-                                       'Precip Driest Quarter',
-                                       'Precip Coldest Quarter'))] <- 'Precip 2'
+                                       'Precip Driest Quarter'))] <- 'Precip 2'
   }
   
-  if(rnr==T){# roaddir <- 'C:\\Users\\bjselige\\Documents\\localdata\\Products_generated_from_Rails_Roads\\'
-    # # roaddir <- 'Q:\\Shared drives\\APHIS  Projects\\shared resources\\data\\Rails_Roads\\Products_generated_from_Rails_Roads\\'
-    rnrdir <- 'C:\\Users\\bjselige\\Desktop\\ne_rnr\\distance\\'
-    rnrvar <- rast(list.files(rnrdir, '.dist.wrld', full.names = T))
-    names(rnrvar) <- c('Road Dist', 'Rail Dist')
-    rnrcl <- data.frame(var=names(rnrvar), cluster='Roadsrails 1')
+  if(elev==T){
+    elevvar <- elevation_global(res=.5, path=geodir)
+    elevcl <- data.frame(var=names(elevvar), cluster='Elevation 1')
   }
   
-  # if(pop==T){
-  #   popvar <- rast('C:\\Users\\bjselige\\Downloads\\gpw-v4-population-density-rev11_2020_30_sec_tif\\gpw_v4_population_density_rev11_2020_30_sec.tif')
-  #   # ghm <- rast('C:\\Users\\bjselige\\Downloads\\lulc-human-modification-terrestrial-systems-geographic-geotiff\\lulc-human-modification-terrestrial-systems_geographic.tif')
-  #   # cpl <- rast('C:\\Users\\bjselige\\Downloads\\ucl_2014_v23.tif')
-  #   names(popvar) <- 'Population Density'
-  #   popcl <- data.frame(var=names(popvar), cluster='Population 1')
-  # }
-  # 
-  # if(crop==T){
-  #   cpldir <- 'C:\\Users\\bjselige\\Desktop\\cropland\\'
-  #   cpl <- cropland("WorldCover", path=cpldir)
-  # }
+  if(gdd==T){
+    if(exists('tbase')==F){tbase <- 5}
+    getGDD <- function(tbase){
+      gdpath <- paste(geodir, 'gdd.base', tbase, '.tif', sep='')
+      if(file.exists(gdpath)){g1 <- rast(gdpath)}  
+      if(!file.exists(gdpath)){require(parallel); print('Calculating GDD')
+        tavg <- worldclim_global(var='tavg', res=.5, path=geodir)
+        g1 <- app(x=tavg, tbase, fun=function(x, tbase){
+          days <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+          xbase <- x-tbase; xbase[which(xbase<0)] <- 0
+          return(sum(xbase*days))}, cores=detectCores()/2)
+        writeRaster(g1, filename=gdpath); closeAllConnections()
+      }
+      names(g1) <- paste('GDD ', tbase, sep=''); return(g1)
+    }
+    gddvar <- getGDD(tbase)
+    gddcl <- data.frame(var=names(gddvar), cluster='Temp 1')
+  }
   
   if(lc==T){
-    lcdir <- 'C:\\Users\\bjselige\\Desktop\\landcover\\'
-    cropl <- landcover(var="cropland", path=lcdir)
-    built <- landcover(var='built', path=lcdir)
-    # trees <- landcover(var='trees', path=lcdir)
-    # shrub <- landcover(var='shrubs', path=lcdir)
-    # grass <- landcover(var='grassland', path=lcdir)
-    # wetld <- landcover(var='wetland', path=lcdir)
-    # snowy <- landcover(var='snow', path=lcdir)
-    # baren <- landcover(var='bare', path=lcdir)
-    lcvar <- c(cropl, built); names(lcvar) <- c('Cropland', 'Built')
-    lcvar <- extend(lcvar, ext(biovar))
+    globe <- worldclim_global(var='bio', res=.5, path=geodir)
+    built <- extend(x=landcover(var='built', path=geodir), y=ext(globe))
+    cropl <- extend(x=landcover(var="cropland", path=geodir), y=ext(globe))
+    grass <- extend(x=landcover(var='grassland', path=geodir), y=ext(globe))
+    shrub <- extend(x=landcover(var='shrubs', path=geodir), y=ext(globe))
+    trees <- extend(x=landcover(var='trees', path=geodir), y=ext(globe))
+    wetld <- extend(x=landcover(var='wetland', path=geodir), y=ext(globe))
+    lcvar <- c(built, cropl, grass, shrub, trees, wetld)
+    names(lcvar) <- c('Built', 'Cropland', 'Grassland', 'Shrubs', 'Trees', 'Wetland')
     lccl <- data.frame(var=names(lcvar), cluster='Landcover 1')
   }
   
+  if(pop==T){
+    popvar <- population(year='2020', res=.5, path=geodir)
+    names(popvar) <- 'Population'
+    popcl <- data.frame(var=names(popvar), cluster='Population 1')
+  }
+  
+  if(ptime==T){
+    getPrecipTiming <- function(){
+      ptpath <- paste(geodir, '\\precip.timing.tif', sep='')
+      if(file.exists(ptpath)){prect <- rast(ptpath)}
+      if(!file.exists(ptpath)){require(parallel); print('Calculating Precip Timing (DJF-JJA)')
+        prec <- worldclim_global(var='prec', res=.5, path=geodir)
+        prect <- app(x=prec, cores=detectCores()/2, 
+                     fun=function(x){
+                       return(sum(x[[12]], x[[1]], x[[2]])-sum(x[[6]], x[[7]], x[[8]]))
+                     })
+        writeRaster(prect, filename=ptpath); closeAllConnections()
+      }
+      names(prect) <- 'Precip Timing (DFJ-JJA)'; return(prect)
+    }
+    timevar <- getPrecipTiming()
+    timecl <- data.frame(var=names(timevar), cluster='Precip 2')
+  }
+  
+  if(rnr==T){
+    rnrvar <- rast(list.files(geodir, '.dist.wrld', full.names = T))
+    names(rnrvar) <- c('Road Dist', 'Rail Dist')
+    rnrcl <- data.frame(var=names(rnrvar), cluster='Roads/Rails 1')
+  }
+  
   if(soil==T){
-    soildir <- 'C:\\Users\\bjselige\\Desktop\\Soil_OpenLandMap\\resample\\'
     soil.files <- c('Soil_pH_0cm.tif', 'Soil_pH_mean.1km.tif', 'Soil_pH_200cm.tif',
                     'Soil_h2o_33kpa_0cm.tif', 'Soil_h2o_33kpa_mean.1km.tif',
                     'Soil_h2o_33kpa_200cm.tif', 'Soil_h2o_1500kpa_0cm.tif',
                     'Soil_h2o_1500kpa_mean.1km.tif', 'Soil_h2o_1500kpa_200cm.tif')
-    solvar <- rast(paste(soildir, soil.files, sep=''))
+    solvar <- rast(paste(geodir, '\\soils\\', soil.files, sep=''))
     names(solvar) <- c('Soil_pH_0cm', 'Soil_pH_mean', 'Soil_pH_200cm',
                        'Soil_h2o_33_0cm', 'Soil_h2o_33_mean', 'Soil_h2o_33_200cm',
                        'Soil_h2o_1500_0cm', 'Soil_h2o_1500_mean', 'Soil_h2o_1500_200cm')
@@ -84,16 +116,19 @@ get_Envi <- function(bio=T, lc=T, rnr=T, soil=T, res=1){#borders=NULL, pop=T, cr
   }
   
   if(bio==F){biovar <- NULL; biocl <- NULL}
+  if(elev==F){elevvar <- NULL; elevcl <- NULL}
+  if(gdd==F){gddvar <- NULL; gddcl <- NULL}
   if(lc==F){lcvar <- NULL; lccl <- NULL}
+  if(pop==F){popvar <- NULL; popcl <- NULL}
+  if(ptime==F){timevar <- NULL; timecl <- NULL}
   if(rnr==F){rnrvar <- NULL; rnrcl <- NULL}
-  #if(pop==F){popvar <- NULL; popcl <- NULL}
   if(soil==F){solvar <- NULL; solcl <- NULL}
   
-  envi <- c(biovar, lcvar, rnrvar, solvar)
-  clst <- rbind(biocl, lccl, rnrcl, solcl)
+  envi <- c(biovar, elevvar, gddvar, lcvar, popvar, timevar, rnrvar, solvar)
+  clst <- rbind(biocl, elevcl, gddcl, lccl, popcl, timecl, rnrcl, solcl)
   clst$cluster <- as.integer(as.factor(clst$cluster))
   cl2 <- clst$cluster; names(cl2) <- gsub(' ', '.', clst$var)
-  if(res>1){envi <- terra::aggregate(envi, fact=res)}
-  # if(!is.null(borders)){envi <- terra::crop(x=envi, y=borders, mask=T)}
+  if(res>1){envi <- terra::aggregate(envi, fact=res, fun='mean')}
+  #if(!is.null(borders)){envi<-terra::crop(x=envi,y=borders,mask=T)}
   return(list('rast'=envi, 'clust'=cl2))
 }
