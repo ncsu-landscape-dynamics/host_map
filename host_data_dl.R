ec43912bdcea25b4f9e56440887d706b032f4918
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2022-12-12 15:11:04 -0500

    included neon & fia

--- host_data_dl.R
+++ host_data_dl.R
@@ -1,4 +1,6 @@
 library(renv)
+init()
+hydrate()
 library(foreach)
 library(rinat)
 
@@ -328,3 +330,310 @@ get_bien_obs<-function(folder, studyext, sn, tax.rank){
     bien.out.c[!is.na(below_sp),]$below_sp<-"Y"
     write.csv(bien.out.c,paste0(folder,sn,"/bien/",sn,".bien.out.clean.csv"), row.names = F)}
   else if(length(bien.out$scrubbed_species_binomial==0)){print("No BIEN records found")}}
+
+#L48 USA: host data downloaders####
+#' @description this function will download the entire FIA SQLite database to the GD
+#' data folder.Then, it will pull out the tree & plot data from this database for the
+#' L48 only. This data will be exported as csvs to the GD folder. A new column will
+#' be added to the tree and plot csvs that indicates when the SQLITE database was last
+#' downloaded. This date will be used to update the FIA SQLite and exported tree &
+#' plot csvs to match the FIA sampling schedule (i.e. bi-annually).
+#' @param path_db the path to the FIA SQLite database on the the shared data drive 
+#' ("Q:/Shared drives/Data/Original/FIA/") or equivalent
+
+#Retrieve ref,tree, & plot data from FIA database & export as csvs
+path_db<-"Q:/Shared drives/Data/Original/FIA/"
+url<-"https://apps.fs.usda.gov/fia/datamart/Databases/SQLite_FIADB_ENTIRE.zip"
+
+con <- DBI::dbConnect(RSQLite::SQLite(), paste0(path_db, "SQLite_FIADB_ENTIRE.db"))
+DBI::dbListTables(con)
+fia.ref<-data.table::as.data.table(DBI::dbGetQuery(con, 'SELECT * FROM REF_SPECIES'))
+write.csv(fia.ref,paste0(path_db,"fia.ref.csv"), row.names = F)
+fia.plot<-data.table::as.data.table(DBI::dbGetQuery(con, 'SELECT CN,MEASYEAR,LAT,LON,STATECD FROM PLOT 
+                                                    WHERE NOT STATECD > 56 AND NOT STATECD=02 AND NOT STATECD=15'))
+fia.tree<-data.table::as.data.table(DBI::dbGetQuery(con, 'SELECT PLT_CN,SPCD FROM TREE 
+                                                    WHERE NOT STATECD > 56 AND NOT STATECD=02 AND NOT STATECD=15'))
+DBI::dbDisconnect(con)
+l48.fia<-unique(merge(fia.tree,fia.plot, by.x="PLT_CN",by.y="CN", all.x=TRUE))
+l48.fia[,retrieved:=Sys.Date()]
+write.csv(l48.fia,paste0(path_db,"l48.fia.csv"), row.names = F)
+rm(fia.plot,fia.tree,l48.fia)
+gc()
+
+#' @description this function will retrieve the FIA data for the *studyext* and *sn*
+#' provided. All absences will be xy points where the *sn* has never been found.
+#' All presences will be the xy points where the *sn* has been found. For absences and presences,
+#' only the most recent survey year will be retained.
+#' @param folder path to a folder to save the exported cleaned host 
+#' occurrence data.
+#' @param studyext a path to raster file that encompasses the area where you would
+#' like to search for occurrence data. Enter "" if you do not want to filter
+#' occurrences by spatial extent.
+#' @param sn the scientific name of the focal host species or genus.
+#' @param tax.rank the taxonomic rank of the *sn* provided. This can only be 
+#' genus or species. If genus, then all species within the genus are retrieved.
+
+#Extract FIA presences & absences for study ext only
+folder<-"Q:/My Drive/PhD/RA/ecff/data/hosts/"
+studyext<-"Q:/My Drive/PhD/RA/ecff/data/ecff_rasters/ecff_2017.tif"
+tax.rank<-"genus"
+sn<-"prunus"
+
+dir.create(paste0(folder,sn))
+dir.create(paste0(folder,sn,"/fia"))
+
+study.area<-terra::rast(studyext)
+study.area<-terra::project(study.area, "+proj=longlat +datum=WGS84 +no_defs +type=crs")
+us<-terra::crop(terra::vect(spData::us_states),study.area)
+us.dt<-data.table::as.data.table(cbind(abb=datasets::state.abb, NAME=datasets::state.name))
+us<-merge(us, us.dt, by="NAME")
+us$GEOID<-as.integer(us$GEOID)
+
+fia.out<-data.table::fread(paste0(path_db,"l48.fia.csv"))
+fia.out<-foreach(i=1:length(us$GEOID),.combine="rbind") %do% 
+  {
+    fia.out[STATECD==us$GEOID[i],]
+  }
+
+fia.ref<-data.table::fread(paste0(path_db,"fia.ref.csv"))
+fia.ref$sciname<-tolower(paste0(fia.ref$GENUS,fia.ref$SPECIES))
+
+if(tax.rank=="genus"){spcd<-unique(fia.ref[tolower(GENUS)==tolower(sn),]$SPCD)}else if(tax.rank=="species"){
+  sn<-tolower(sn(gsub(" ","",sn)))
+  spcd<-unique(fia.ref[sciname==tolower(sn),]$SPCD)}else{print("Confirm that tax.rank=genus or species")}
+
+if(length(spcd)==0){stop("taxa scientific name not found in FIA reference table")}
+
+fia.out.p<-foreach(i=1:length(spcd), .combine="rbind") %do% {
+  x<-fia.out[SPCD==spcd[i],]
+  if(length(x$PLT_CN)>0){
+    x<-unique(x[,.(year=max(MEASYEAR),p_a=1, lat=LAT, lon=LON, db="fia",
+                   spcd=spcd[i], retrieved=retrieved,fkey=NA),by=.(LAT,LON)])
+    x[,-c(1:2)]}
+  else{}
+}
+
+if(length(fia.out.p$lat)==0){stop("no occurrences of taxa found in FIA database")
+  }else{spcd<-unique(fia.out.p$spcd)}
+
+#Pull out absences & most recent date surveyed per plot
+fia.out.a<-foreach(i=1:length(spcd),.combine="rbind") %do% {
+  x<-fia.out[SPCD!=spcd[i],]
+  if(length(x$PLT_CN)>0){
+    x<-unique(x[,.(year=max(MEASYEAR),p_a=0, lat=LAT, lon=LON, db="fia",
+                   spcd=spcd[i],retrieved=retrieved,fkey=NA), by=.(LAT,LON)])
+    x[,-c(1:2)]}
+  else{}
+}
+
+fia.out.a<-unique(dplyr::anti_join(fia.out.a,fia.out.p, by=c("lat","lon","spcd")))
+fia.out.a<-data.table::as.data.table(fia.out.a)
+fia.out.c<-rbind(fia.out.a,fia.out.p)
+fia.out.c<-merge(fia.out.c,fia.ref, by.x="spcd",by.y="SPCD")
+fia.out.c$below_sp<-""
+fia.out.c[is.na(VARIETY)|is.na(SUBSPECIES),]$below_sp<-"N"
+fia.out.c[!is.na(VARIETY)|!is.na(SUBSPECIES),]$below_sp<-"Y"
+
+fia.out.c<-fia.out.c[,.(year,p_a,lat,lon,db,genus=GENUS,species=SPECIES, 
+                        variety=VARIETY,subspecies=SUBSPECIES, retrieved,fkey,below_sp)]
+fia.out.c[is.na(variety)&is.na(subspecies),dbname:=paste0(genus," ",species)]
+fia.out.c[is.na(variety)&!is.na(subspecies),dbname:=paste0(genus," ",species," ",subspecies)]
+fia.out.c[!is.na(variety)&is.na(subspecies),dbname:=paste0(genus," ",species," ",variety)]
+fia.out.c[species=="spp.",]$species<-NA
+fia.out.c<-fia.out.c[,.(year,p_a,lat,lon,db,dbname,genus,species,retrieved,fkey,below_sp)]
+
+write.csv(fia.out.c,paste0(folder,sn,"/fia/",sn,".fia.out.clean.csv"), row.names = F)
+
+#NEON####
+#' @description this function will download the entire NEON  database to the GD
+#' data folder.
+#' @param path_db the path to the NEON database on the the shared data drive 
+#' ("Q:/Shared drives/Data/Original/NEON/") or equivalent
+
+dir.create("Q:/Shared drives/Data/Original/NEON/")
+path_db<-"Q:/Shared drives/Data/Original/NEON/"
+neon.out <- neonUtilities::loadByProduct(dpID="DP1.10058.001", site="all", package="expanded")
+neon.out$div_10m2Data100m2Data$retrieved<-Sys.Date()
+neon.out$div_1m2Data$retrieved<-Sys.Date()
+write.csv(neon.out$div_10m2Data100m2Data,paste0(path_db,"div_10m2Data100m2Data.csv"),row.names = F)
+write.csv(neon.out$div_1m2Data,paste0(path_db,"div_1m2Data.csv"), row.names = F)
+
+#' @description this function will retrieve the NEON data for the *studyext* and *sn*
+#' provided. All absences will be xy points where the *sn* has never been found.
+#' All presences will be the xy points where the *sn* has been found. For absences and presences,
+#' only the most recent survey year will be retained.
+#' @param folder path to a folder to save the exported cleaned host 
+#' occurrence data.
+#' @param studyext a path to raster file that encompasses the area where you would
+#' like to search for occurrence data. Enter "" if you do not want to filter
+#' occurrences by spatial extent.
+#' @param sn the scientific name of the focal host species or genus.
+#' @param tax.rank the taxonomic rank of the *sn* provided. This can only be 
+#' genus or species. If genus, then all species within the genus are retrieved.
+
+folder<-"Q:/My Drive/PhD/RA/ecff/data/hosts/"
+studyext<-"Q:/My Drive/PhD/RA/ecff/data/ecff_rasters/ecff_2017.tif"
+tax.rank<-"genus"
+sn<-"lonicera"
+
+dir.create(paste0(folder,sn))
+dir.create(paste0(folder,sn,"/neon"))
+
+study.area<-terra::rast(studyext)
+study.area<-terra::project(study.area, "+proj=longlat +datum=WGS84 +no_defs +type=crs")
+study.area<-terra::ext(study.area)
+
+neon.out<-data.table::fread(paste0(path_db,"div_10m2Data100m2Data.csv"))
+neon.out<-neon.out[decimalLongitude>=study.area[1] & decimalLongitude<=study.area[2] &
+           decimalLatitude>=study.area[3] & decimalLatitude<=study.area[4],]
+neon.sp<-unique(neon.out$scientificName)
+neon.out$publicationDate<-lubridate::ymd_hms(neon.out$publicationDate)
+
+if(tax.rank=="genus"){
+  sn2<-stringr::str_to_title(sn)
+  neon.sp<-neon.sp[grep(sn2,neon.sp,ignore.case = F)]
+}else if(tax.rank=="species"){
+  neon.sp<-neon.sp[grep(sn,neon.sp,ignore.case = F)]
+  }else{print("Confirm that tax.rank=genus or species")}
+
+if(length(neon.sp)==0){stop("taxa scientific name not found in the NEON database")}
+
+neon.out.p<-foreach(i=1:length(neon.sp), .combine="rbind") %do% {
+  x<-neon.out[scientificName==neon.sp[i],]
+  if(length(x$uid)>0){
+    x<-unique(x[,.(year=max(lubridate::year(publicationDate)),p_a=1, lat=decimalLatitude, 
+                   lon=decimalLongitude, db="neon",dbname=neon.sp[i], retrieved=retrieved, 
+                   fkey=NA, species=neon.sp[i],taxonRank),
+                by=.(decimalLatitude,decimalLongitude)])
+    x[,-c(1:2)]}
+  else{}
+}
+
+if(length(neon.out.p$lat)==0){stop("no occurrences of taxa found in FIA database")
+}else{neon.sp<-unique(cbind(sn=neon.out.p$species,tr=neon.out.p$taxonRank))}
+
+#Pull out absences & most recent date surveyed per plot
+neon.out.a<-foreach(i=1:length(neon.sp[,1]), .combine="rbind") %do% {
+  x<-neon.out[scientificName!=neon.sp[i,1],]
+  if(length(x$uid)>0){
+    x<-unique(x[,.(year=max(lubridate::year(publicationDate)),p_a=0, lat=decimalLatitude, 
+                   lon=decimalLongitude, db="neon",dbname=neon.sp[i,1], retrieved=retrieved, 
+                   fkey=NA, species=neon.sp[i,1],taxonRank=neon.sp[i,2]),
+                by=.(decimalLatitude,decimalLongitude)])
+    x[,-c(1:2)]}
+  else{}
+}
+
+neon.out.a<-unique(dplyr::anti_join(neon.out.a,neon.out.p, by=c("lat","lon","species")))
+neon.out.a<-data.table::as.data.table(neon.out.a)
+neon.out.c<-rbind(neon.out.a,neon.out.p)
+neon.out.c$below_sp<-""
+neon.out.c[taxonRank!="genus"&taxonRank!="species",]$below_sp<-"Y"
+neon.out.c[taxonRank=="genus"|taxonRank=="species",]$below_sp<-"N"
+sp<-strsplit(neon.out.c$species," ")
+sp<-lapply(sp,function(x) cbind(genus=x[1],species=x[2]))
+sp<-data.table::as.data.table(do.call("rbind",sp))
+neon.out.c$species<-sp$species
+neon.out.c$genus<-sp$genus
+neon.out.c[taxonRank=="genus"]$species<-NA
+neon.out.c<-unique(neon.out.c[,.(year,p_a,lat,lon,db,genus,species,retrieved,fkey,dbname,below_sp)])
+
+write.csv(neon.out.c,paste0(folder,sn,"/neon/",sn,".neon.out.clean.csv"), row.names = F)
+
+#BLM####
+#' @description this function will download the entire BLM AIM database to the GD
+#' data folder.
+#' @param path_db the path to the NEON database on the the shared data drive 
+#' ("Q:/Shared drives/Data/Original/BLM_AIM_terradat/") or equivalent
+url<-"https://gbp-blm-egis.hub.arcgis.com/datasets/BLM-EGIS::blm-natl-aim-terradat-hub.csv?outSR=%7B%22latestWkid%22%3A4269%2C%22wkid%22%3A4269%7D"
+dir.create("Q:/Shared drives/Data/Original/BLM_AIM_terradat/")
+path_db<-"Q:/Shared drives/Data/Original/BLM_AIM_terradat/"
+download.file(url=url,destfile = paste0(path_db,"BLM_Natl_AIM_TerrADat_Hub.csv"),mode = "w")
+
+url<-"https://plantsorig.sc.egov.usda.gov/Data/plantlst.txt"
+download.file(url=url,destfile = paste0(path_db,"plantlst.txt"),mode = "w")
+
+#' @description this function will retrieve the BLM data for the *studyext* and *sn*
+#' provided. All absences will be xy points where the *sn* has never been found.
+#' All presences will be the xy points where the *sn* has been found. For absences and presences,
+#' only the most recent survey year will be retained.
+#' @param folder path to a folder to save the exported cleaned host 
+#' occurrence data.
+#' @param studyext a path to raster file that encompasses the area where you would
+#' like to search for occurrence data. Enter "" if you do not want to filter
+#' occurrences by spatial extent.
+#' @param sn the scientific name of the focal host species or genus.
+#' @param tax.rank the taxonomic rank of the *sn* provided. This can only be 
+#' genus or species. If genus, then all species within the genus are retrieved.
+
+folder<-"Q:/My Drive/PhD/RA/ecff/data/hosts/"
+studyext<-"Q:/My Drive/PhD/RA/ecff/data/ecff_rasters/ecff_2017.tif"
+tax.rank<-"genus"
+sn<-"prunus"
+
+dir.create(paste0(folder,sn))
+dir.create(paste0(folder,sn,"/blm/"))
+
+study.area<-terra::rast(studyext)
+study.area<-terra::project(study.area, "+proj=longlat +datum=WGS84 +no_defs +type=crs")
+study.area<-terra::ext(study.area)
+
+blm.out<-data.table::fread(paste0(path_db,"BLM_Natl_AIM_TerrADat_Hub.csv"))
+usda<-data.table::fread(paste0(path_db,"plantlst.txt"))
+
+
+#blm.out<-blm.out[X>=study.area[1] & X<=study.area[2] & Y>=study.area[3] & Y<=study.area[4],]
+head(blm.out)
+neon.sp<-unique(blm.out$)
+neon.out$publicationDate<-lubridate::ymd_hms(neon.out$publicationDate)
+
+if(tax.rank=="genus"){
+  sn2<-stringr::str_to_title(sn)
+  neon.sp<-neon.sp[grep(sn2,neon.sp,ignore.case = F)]
+}else if(tax.rank=="species"){
+  neon.sp<-neon.sp[grep(sn,neon.sp,ignore.case = F)]
+}else{print("Confirm that tax.rank=genus or species")}
+
+if(length(neon.sp)==0){stop("taxa scientific name not found in the NEON database")}
+
+neon.out.p<-foreach(i=1:length(neon.sp), .combine="rbind") %do% {
+  x<-neon.out[scientificName==neon.sp[i],]
+  if(length(x$uid)>0){
+    x<-unique(x[,.(year=max(lubridate::year(publicationDate)),p_a=1, lat=decimalLatitude, 
+                   lon=decimalLongitude, db="neon",dbname=neon.sp[i], retrieved=retrieved, 
+                   fkey=NA, species=neon.sp[i],taxonRank),
+                by=.(decimalLatitude,decimalLongitude)])
+    x[,-c(1:2)]}
+  else{}
+}
+
+if(length(neon.out.p$lat)==0){stop("no occurrences of taxa found in FIA database")
+}else{neon.sp<-unique(cbind(sn=neon.out.p$species,tr=neon.out.p$taxonRank))}
+
+#Pull out absences & most recent date surveyed per plot
+neon.out.a<-foreach(i=1:length(neon.sp[,1]), .combine="rbind") %do% {
+  x<-neon.out[scientificName!=neon.sp[i,1],]
+  if(length(x$uid)>0){
+    x<-unique(x[,.(year=max(lubridate::year(publicationDate)),p_a=0, lat=decimalLatitude, 
+                   lon=decimalLongitude, db="neon",dbname=neon.sp[i,1], retrieved=retrieved, 
+                   fkey=NA, species=neon.sp[i,1],taxonRank=neon.sp[i,2]),
+                by=.(decimalLatitude,decimalLongitude)])
+    x[,-c(1:2)]}
+  else{}
+}
+
+neon.out.a<-unique(dplyr::anti_join(neon.out.a,neon.out.p, by=c("lat","lon","species")))
+neon.out.a<-data.table::as.data.table(neon.out.a)
+neon.out.c<-rbind(neon.out.a,neon.out.p)
+neon.out.c$below_sp<-""
+neon.out.c[taxonRank!="genus"&taxonRank!="species",]$below_sp<-"Y"
+neon.out.c[taxonRank=="genus"|taxonRank=="species",]$below_sp<-"N"
+sp<-strsplit(neon.out.c$species," ")
+sp<-lapply(sp,function(x) cbind(genus=x[1],species=x[2]))
+sp<-data.table::as.data.table(do.call("rbind",sp))
+neon.out.c$species<-sp$species
+neon.out.c$genus<-sp$genus
+neon.out.c[taxonRank=="genus"]$species<-NA
+neon.out.c<-unique(neon.out.c[,.(year,p_a,lat,lon,db,genus,species,retrieved,fkey,dbname)])
+
+write.csv(neon.out.c,paste0(folder,sn,"/neon/",sn,".neon.out.clean.csv"), row.names = F)
